9d259842a42e66a0c09f316b260c292fb1662e8e
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-02 20:09:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,13 +7,13 @@ def map_to_negativize(array)
   return array
 end
 
-def map_to_no_change(source_array)
+def map_to_no_change(array)
 end 
 
-def map_to_double(source_array)
+def map_to_double(array)
 end 
 
-def map_to_square(source_array)
+def map_to_square(array)
 end 
 
 def reduce_to_total(source_array, starting_point = 0)
