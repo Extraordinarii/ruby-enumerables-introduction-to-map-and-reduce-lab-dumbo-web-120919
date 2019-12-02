b5a5b7d9adb42a85e3df051cffeb1fab3ce94a49
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-02 20:04:48 +0000

    Automatically backed up by Learn

---
+++ lib/my_code.rb
@@ -0,0 +1,17 @@
+def map_to_negativize(source,array)
+end 
+
+def map_to_no_change(source_array)
+end 
+
+def map_to_double(source_array)
+end 
+
+def map_to_square(source_array)
+end 
+
+def reduce_to_total(source_array, starting_point = 0)
+end 
+
+def reduce_to_all_true(source_array, starting_point = 0 )
+end 
