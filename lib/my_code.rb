d85daa4edbbee6f34f818dd4ee788f10e0af035b
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-02 20:05:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,3 +15,11 @@ end
 
 def reduce_to_all_true(source_array, starting_point = 0 )
 end 
+
+def reduce_to_any_true(source_array, starting_point = 0)
+  while i< source_array.length do 
+    return true if source_array[i]
+    i += 1 
+  end 
+  false
+end 
